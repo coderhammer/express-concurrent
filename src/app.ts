b3a893b695a38394a4cf0cb70635b5d4ec362d78
coderhammer <122128585+coderhammer@users.noreply.github.com>
2023-07-04 19:49:04 +0000

    feat: express middleware

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,58 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 const app = express();
 
-const queue: { req: Request; res: Response }[] = [];
+const createMiddleware = (options: { maxConcurrentRequests: number }) => {
+  const queue: NextFunction[] = [];
 
-const running = new Set<{ req: Request; res: Response }>();
+  const running = new Set<NextFunction>();
 
-const maxConcurrentRequests = 2;
+  const maxConcurrentRequests = options.maxConcurrentRequests;
 
-const handler = async (req: Request, res: Response) => {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
-  console.log("handling request!");
-  res.send("ok");
+  const processQueue = () => {
+    if (!queue.length) {
+      console.log("Queue is empty.");
+      return;
+    }
+    if (running.size >= maxConcurrentRequests) {
+      console.log(
+        `Running a maximum of ${maxConcurrentRequests}, will handle later`
+      );
+      return;
+    }
+    const next = queue.shift();
+    running.add(next);
+    next();
+  };
+
+  const queueMiddleware = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    req.on("close", () => {
+      console.log("the request is over.");
+      running.delete(next);
+      processQueue();
+    });
+    queue.push(next);
+    processQueue();
+  };
+
+  return queueMiddleware;
 };
 
-const processQueue = async () => {
-  console.log("processing queue");
-  if (!queue.length) {
-    console.log("queue is empty");
-    return;
-  }
-  if (running.size >= maxConcurrentRequests) {
-    console.log("queue is full, will handle later");
-    return;
-  }
-  const tuppleToProcess = queue.shift();
-  running.add(tuppleToProcess);
-  await handler(tuppleToProcess.req, tuppleToProcess.res);
-  running.delete(tuppleToProcess);
-  return processQueue();
-};
+app.use(
+  createMiddleware({
+    maxConcurrentRequests: 2,
+  })
+);
 
 app.get("/", async (req: Request, res: Response) => {
-  queue.push({ req, res });
-  processQueue();
+  req.setTimeout(10000);
+  await new Promise((resolve) => setTimeout(resolve, 20000));
+  console.log("handling request!");
+  res.send("ok");
 });
 
 app.listen(3000, () => {
